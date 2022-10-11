36e3c562827d09c1c2f5079c463abd79b928408a
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 04:38:04 +0000

    7lab4

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ for i in range(10):
 	numbers2.append(random.randint(0,100))
 print (numbers1)
 print (numbers2)
+for j in range(10):
+	w = numbers1[j] + numbers2[j]
+	result.append(w) 
+print(result)
